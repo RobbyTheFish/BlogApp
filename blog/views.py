6d054cf0bd485e69d34e00167fc33e1b4aadfde4
FreezingMoon <oyaroslav2004@gmail.com>
2021-07-23 18:28:09 +0300

    Created blog page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'index.html'
+    context_object_name = 'all_posts_list'
 # Create your views here.
