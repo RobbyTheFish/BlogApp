3308e94f53ee7c0fa94a3df6c02ab798778a7e8e
FreezingMoon <oyaroslav2004@gmail.com>
2021-07-28 19:42:56 +0300

    Added unit-tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
+from .models import Post
+
+class BlogTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='test_user',
+            email='test@mail.com',
+            password='secret'
+        )
+        self.post = Post.objects.create(
+            title='A good title',
+            body='Nice body content',
+            author=self.user
+        )
+
+    def test_spring_representation(self):
+        post = Post(title='A sample title')
+        self.assertEqual(str(post), post.title)
+
+    def test_post_content(self):
+        self.assertEqual(f'{self.post.title}', 'A good title')
+        self.assertEqual(f'{self.post.body}', 'Nice body content')
+        self.assertEqual(f'{self.post.author}', 'test_user')
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Nice body content')
+        self.assertTemplateUsed(response, 'index.html')
+
+    def post_detail_view(self):
+        response = self.client.get('/post/1')
+        no_response = self.client.get('/post/100000')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 404)
+        self.assertContains(response, 'A good title')
+        self.assertTemplateUsed(response, 'post_detail.html')
 # Create your tests here.
